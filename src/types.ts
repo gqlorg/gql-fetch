fc1dc3fbf3a7b3b63cab285b55b7460cc5505658
ahincalan <ahincalan@hotmail.com>
2019-04-19 15:13:42 +0300

    Changed IPromiseResolved type.

--- src/types.ts
+++ src/types.ts
@@ -23,9 +23,6 @@ export interface IQueryVariables {
     [key: string]: any;
 }
 
-export type IPromiseResolved<T = undefined> =
-    ((value: T) => void | undefined | any | PromiseLike<any>)
-    | undefined
-    | null;
+export type IPromiseResolved<T> = ((value: T) =>  void | undefined | any | PromiseLike<any>) | undefined | null;
 export type IPromiseRejected = ((reason: any) => never | PromiseLike<never>) | undefined | null;
 export type IPromiseFinally = (() => void) | undefined | null;
