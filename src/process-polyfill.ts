d17a885a91368d5097cc5ff39a379a7514c2e7d6
ahincalan <ahincalan@hotmail.com>
2019-04-19 13:56:09 +0300

    Added process polyfill.

--- src/process-polyfill.ts
+++ src/process-polyfill.ts
@@ -1,6 +1,8 @@
+/* istanbul ignore next */
 // @ts-ignore
 const root = (typeof self === 'object' && self.window) || global;
 
+// Process polyfill
 // @ts-ignore
 if (!root.process) {
     // @ts-ignore
